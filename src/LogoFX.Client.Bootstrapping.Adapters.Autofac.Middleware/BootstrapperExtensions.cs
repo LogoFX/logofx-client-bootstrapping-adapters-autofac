abe657bcee36f5875cdb8808f43472d321327b1d
godrose <godrose@gmail.com>
2016-04-20 19:53:00 +0300

    Added autofac container registration middleware.

---
+++ src/LogoFX.Client.Bootstrapping.Adapters.Autofac.Middleware/BootstrapperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using LogoFX.Bootstrapping;
+
+namespace LogoFX.Client.Bootstrapping.Adapters.Autofac
+{
+    /// <summary>
+    /// The bootstrapper extension methods.
+    /// </summary>
+    public static class BootstrapperExtensions
+    {
+        /// <summary>
+        /// Uses the autofac container registration.
+        /// </summary>        
+        /// <param name="bootstrapper">The bootstrapper.</param>
+        public static IBootstrapperWithContainer<AutofacAdapter, ContainerBuilder> UseAutofacContainerRegistration(
+            this IBootstrapperWithContainer<AutofacAdapter, ContainerBuilder> bootstrapper)
+        {
+            return bootstrapper.Use(new RegisterAutofacContainerMiddleware());
+        }
+    }
+}
---
+++ src/LogoFX.Client.Bootstrapping.Adapters.Autofac.Middleware/Middleware.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Autofac;
+using LogoFX.Bootstrapping;
+using LogoFX.Core;
+using Solid.Practices.Middleware;
+
+namespace LogoFX.Client.Bootstrapping.Adapters.Autofac
+{
+    /// <summary>
+    /// Builds and registers autofac ioc container as a lifetime scope.
+    /// </summary>    
+    public class RegisterAutofacContainerMiddleware : IMiddleware<IBootstrapperWithContainer<AutofacAdapter, ContainerBuilder>>
+    {
+        /// <summary>
+        /// Applies the middleware on the specified object.
+        /// </summary>
+        /// <param name="object">The object.</param>
+        /// <returns/>
+        public IBootstrapperWithContainer<AutofacAdapter, ContainerBuilder> Apply(IBootstrapperWithContainer<AutofacAdapter, ContainerBuilder> @object)
+        {
+            EventHandler strongHandler = ObjectOnInitializationCompleted;
+            @object.InitializationCompleted += WeakDelegate.From(strongHandler);
+            return @object;
+        }
+
+        private void ObjectOnInitializationCompleted(object sender, EventArgs eventArgs)
+        {
+            var bootstrapper = sender as IBootstrapperWithContainer<AutofacAdapter, ContainerBuilder>;
+            if (bootstrapper != null)
+            {
+                bootstrapper.Container.RegisterInstance(bootstrapper.Container);
+                var container = bootstrapper.Container.Build();
+                bootstrapper.Container.RegisterInstance(container);
+                var lifetimeScope = container.BeginLifetimeScope();
+                bootstrapper.Container.RegisterInstance(lifetimeScope);                
+            }            
+        }
+    }
+}
